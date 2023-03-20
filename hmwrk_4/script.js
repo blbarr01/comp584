b64b551a95c35e5ea7e28873abe00130ccaebea6
Brian <bbarr0101@gmail.com>
2023-03-20 12:23:54 -0700

    fixes uneveness and responsiveness at tablet layout

--- hmwrk_4/script.js
+++ hmwrk_4/script.js
@@ -13,6 +13,7 @@ fetch('https://dummyjson.com/products') //return http request object
 
 
 function loadContainer(products){
+    // the global index helps me parse throught the array of products accross calls 
     let gi = 0; 
     let singleCols = Object.values($(".col-1")); 
     let doubleCols = Object.values($(".col-2"));
@@ -26,9 +27,11 @@ function loadContainer(products){
             for(let j = 0; j <count; j++ ){
                 content = `
                 <div class="product-card">
-                    <img src="${products[gi].images[0]}" alt="">
-                    <h3 class="product-name"> ${products[gi].title}:<span> $ ${products[gi].price}</span> </h3>
-                    <p class="product-desc">${products[gi].description}</p>
+                    <img src="${products[gi].images[0]}" alt="${products[gi].title}">
+                    <div class="product-details"> 
+                        <h3 class="product-name"> ${products[gi].title}:<span> $ ${products[gi].price}</span> </h3>
+                        <p class="product-desc">${products[gi].description}</p>
+                    </div>   
                 </div>   
                 `;
                 gi++; 
@@ -37,18 +40,9 @@ function loadContainer(products){
         }
     }
 
-
     renderProducts(singleCols, 1);
     renderProducts(doubleCols, 1);
     renderProducts(trippleCols, 1);
-    renderProducts(quatrainCols, 2);
-
-
-
-
-
-
-
-
+    renderProducts(quatrainCols, 1);
 
 }
